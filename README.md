0e4ddebd4b258ac0458bd9cf21beed77b82ea72d
savechina <weirenyan0@gmail.com>
2014-04-21 14:42:40 +0800

    init code

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-rendb
+RenDB
 =====
---
+++ src/main/java/org/renyan/rendb/RenDB.java
@@ -0,0 +1,10 @@
+package org.renyan.rendb;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: weirenan
+ * Date: 14-4-21
+ * Time: 下午2:34
+ */
+public interface RenDB {
+}
